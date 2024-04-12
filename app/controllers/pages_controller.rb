5913e42e1205909c475bc1a31ec5a7555331159e
Joel Joseph <joeljosephpam@gmail.com>
2024-04-12 16:58:18 +0530

    Home page now dynamically loads in the posts also shows meta info such x hour ago css needs crtical fixture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @main_posts = Post.limit(4).where("created_at >= ?", 3.hours.ago)
+    @secondary_posts = Post.offset(3).limit(5)
   end
 
   def contact
