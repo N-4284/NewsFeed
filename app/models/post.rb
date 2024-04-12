d56b5889aa9c6fd152aff4f0e069b43e206c0039
Joel Joseph <joeljosephpam@gmail.com>
2024-04-12 13:40:38 +0530

    thumbnail validation implemented changed bg color

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,30 @@ class Post < ApplicationRecord
     belongs_to :category
     belongs_to :location
 
-    #has_one_attached :thumbnail
     has_one_attached :thumbnail, dependent: :purge_later, service: :local
-    #problem causer^^ (i have old db which stores thumb at assets\images\thumbnails or somthin)
+    validate :correct_thumbnail_type
+    validate :thumbnail_size
+
+
+    private  
+    
+    def correct_thumbnail_type
+        unless thumbnail.attached?
+            errors.add(:thumbnail, 'must be present')
+        end
+        if thumbnail.attached? && !thumbnail.content_type.in?(%w(image/png image/jpeg))
+          errors.add(:thumbnail, 'must be a PNG, JPG or JPEG file')
+          thumbnail.purge # Delete the attached file
+        end
+    end
+
+    def thumbnail_size
+        if thumbnail.attached?
+          if thumbnail.blob.byte_size > 3.megabytes
+            errors.add(:thumbnail, 'must be less than 5MB')
+            thumbnail.purge  # Remove the attachment
+          end        
+        end
+    end
+
 end
