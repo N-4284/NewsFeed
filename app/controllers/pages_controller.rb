b3507e30fdc8ec4a0dedc8d755d087c0e452c56b
Joel Joseph <joeljosephpam@gmail.com>
2024-04-17 10:27:43 +0530

    home page now dynamically loads content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
   def home
-    @main_posts = Post.limit(4).where("created_at >= ?", 3.hours.ago)
-    @secondary_posts = Post.offset(3).limit(5)
+    @recent_posts = Post.limit(4).where("created_at >= ?", 1.hours.ago)
+    @secondary_posts = @recent_posts.present? ? Post.where.not(id: @recent_posts.pluck(:id)).limit(@recent_posts.count * 2) : Post.limit(4)
+    @tech = Post.joins(:category).where(categories: { title: 'technology' })
+    
+    @categories_with_posts = Category.joins(:post)
+    .group('categories.id')
+    .having('COUNT(posts.id) >= 2').limit(4)
+
+    @locations_with_posts = Location.joins(:post)
+    .group('locations.id')
+    .having('COUNT(posts.id) >= 2').limit(4)
+
   end
 
   def contact
